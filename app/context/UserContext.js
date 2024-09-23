0692ec0734ef608f6003515104cce0abfda63dea
Smiffeed <97792035+Smiffeed@users.noreply.github.com>
2024-09-23 21:45:58 +0700

    Refactor(Backend): GET from route.js user instead of context(v.2)

--- app/context/UserContext.js
+++ app/context/UserContext.js
@@ -5,13 +5,6 @@ const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
-
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
