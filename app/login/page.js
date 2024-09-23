9caa3a81e340407ab02ff8e27b5e767d15fb28b8
Smiffeed <97792035+Smiffeed@users.noreply.github.com>
2024-09-23 21:59:06 +0700

    Fix(Backend): Login button not post fixed

--- app/login/page.js
+++ app/login/page.js
@@ -15,7 +15,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('/api/users', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
