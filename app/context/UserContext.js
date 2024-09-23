55bb0798c9cdfaaf34262e270ac8cac5f71cdadb
Smiffeed <97792035+Smiffeed@users.noreply.github.com>
2024-09-23 22:04:56 +0700

    Fix(Frontend): UserNav reload update fixed

--- app/context/UserContext.js
+++ app/context/UserContext.js
@@ -5,6 +5,13 @@ const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
