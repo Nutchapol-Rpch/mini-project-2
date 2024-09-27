abafeaa26ea72a81834148b6cdb8a8a3b6dc1163
Beingka-source <117561156+MrShojiChin@users.noreply.github.com>
2024-09-27 18:09:13 +0700

    Refactor: Make UserContext more robust in Vercel

--- app/context/UserContext.js
+++ app/context/UserContext.js
@@ -7,22 +7,25 @@ export const UserProvider = ({ children }) => {
 
   useEffect(() => {
     const fetchUser = async () => {
-      const storedUser = localStorage.getItem('user');
-      if (storedUser) {
-        const parsedUser = JSON.parse(storedUser);
-        try {
-          const userResponse = await fetch(`/api/users?userId=${parsedUser._id}`);
-          if (userResponse.ok) {
-            const userData = await userResponse.json();
-            setUser({
-              ...userData.user,
-              lastEditedAt: userData.user.lastEditedAt,
-              profilePicture: userData.user.profilePicture
-            });
-            localStorage.setItem('user', JSON.stringify(userData.user));
+      // Check if window is defined to ensure we're on the client side
+      if (typeof window !== 'undefined') {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+          const parsedUser = JSON.parse(storedUser);
+          try {
+            const userResponse = await fetch(`/api/users?userId=${parsedUser._id}`);
+            if (userResponse.ok) {
+              const userData = await userResponse.json();
+              setUser({
+                ...userData.user,
+                lastEditedAt: userData.user.lastEditedAt,
+                profilePicture: userData.user.profilePicture
+              });
+              localStorage.setItem('user', JSON.stringify(userData.user));
+            }
+          } catch (error) {
+            console.error('Failed to fetch user data:', error);
           }
-        } catch (error) {
-          console.error('Failed to fetch user data:', error);
         }
       }
     };
